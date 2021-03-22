d32baf5da7f9df11530ad66f42167c64dbd1d4de
Kalan Prudhomme <cleaver9@gmail.com>
2021-03-22 13:41:54 -0700

    added button

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import functions and grab DOM elements
-
+const college = document.getElementById('input-1');
+const species = document.getElementById('input-2');
 // initialize state
 
 // set event listeners to update state and DOM
