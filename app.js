64aa3057cda0b2adc414d36a1ff1dffd72df0c1d
NickDayFSD <80706339+NickDayFSD@users.noreply.github.com>
2021-03-22 18:07:55 -0700

    removed error loop and styled website

--- app.js
+++ app.js
@@ -35,45 +35,38 @@ const button = document.getElementById('button');
 
 // set event listeners to update state and DOM
 
-if (document.getElementById('inputs').value === null) {
-    alert ('Fill in the blank!');
-}
-
-
 button.addEventListener('click', ()=> {
+
+    //error message for blank
+    
+
     const div = document.getElementById('classified');
     const section = document.getElementById('inputs');
-
-
-   // if (document.getElementsByTagName('input').value === '') {
-   //     alert ('Fill in the blank!');
-   // } else {
-    const allInputs = document.querySelectorAll('input');
-    for (let i = 0; i < 14; i++) {
-        const val = allInputs[i].value;
-        if (val === '') {
-            return alert('Fill in the Blank');
-        } else {
-
-            display1.textContent = input1.value;
-            display2.textContent = input2.value;
-            display3.textContent = input3.value;
-            display4.textContent = input4.value;
-            display5.textContent = input5.value;
-            display6.textContent = input6.value;
-            display7.textContent = input7.value;
-            display8.textContent = input8.value;
-            display9.textContent = input9.value;
-            display10.textContent = input10.value;
-            display11.textContent = input11.value;
-            display12.textContent = input12.value;
-            display13.textContent = input13.value;
-            display14.textContent = input14.value;
     
-            div.classList.toggle('classified');
-            section.classList.toggle('top-secret');
-        }
-    }
-}
-);
+    display1.textContent = input1.value;
+    display2.textContent = input2.value;
+    display3.textContent = input3.value;
+    display4.textContent = input4.value;
+    display5.textContent = input5.value;
+    display6.textContent = input6.value;
+    display7.textContent = input7.value;
+    display8.textContent = input8.value;
+    display9.textContent = input9.value;
+    display10.textContent = input10.value;
+    display11.textContent = input11.value;
+    display12.textContent = input12.value;
+    display13.textContent = input13.value;
+    display14.textContent = input14.value
+    div.classList.toggle('classified');
+    section.classList.toggle('top-secret');
+});
 
+/*tried to implement
+const allInputs = document.querySelectorAll('input');
+for (let i = 0; i < 14; i++) {
+    const val = allInputs[i].value;
+    console.log(val);
+    if (val === '') {
+        return alert('Fill in the Blank');
+    } else {
+    }*/
